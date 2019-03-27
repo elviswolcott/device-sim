72ea28caf6800c3dae1461363ce4eac7d8ede914
Elvis <elviswolcott@gmail.com>
2019-03-26 22:59:19 -0500

    initial commit

---
+++ scripts/controller.js
@@ -0,0 +1,104 @@
+const device_max = 5;
+var model = new Vue({
+  el: '#app',
+  data: {
+    devices: 0,
+    selected: 0,
+    currentWidth: 0,
+    currentHeight: 0,
+    currentScale: 1,
+    currentPage: '',
+    presets: [{
+      name: "iPhone 6/6s",
+      height: 667,
+      width: 375,
+      scale: 1
+    }],
+    selectedPreset: '',
+    added: []
+  },
+  methods: {
+    newDevice: async function() {
+      console.log('creating new device');
+      // limit to device_max
+      if (this.$data.devices > device_max) {
+        return;
+      }
+
+      // add a new device
+      this.$data.devices++;
+      this.$data.added.push({
+        width:0, height:0, scale:1, page: '',
+      });
+      // spawn the window
+      await restore(`device_${this.$data.devices}`);
+      // select it
+      this.$data.selected = this.$data.devices;
+      this.$data.currentHeight = 0;
+      this.$data.currentWidth = 0;
+      this.$data.currentScale = 1;
+      this.$data.currentPage = '';
+      this.$data.selectedPreset = '';
+      // set the number in the window
+      const window = (await getOpenWindows())[`device_${this.$data.selected}`];
+      console.log(window);
+      window.DEVICE_NUMBER = this.$data.selected;
+    },
+    applyPreset: function(p) {
+      console.log("applying preset: ", p);
+      this.$data.currentWidth = p.width;
+      this.$data.currentHeight = p.height;
+      this.$data.currentScale = p.scale;
+      this.$data.selectedPreset = '';
+    },
+    navigate: async function() {
+      const window = (await getOpenWindows())[`device_${this.$data.selected}`];
+      const frame = window.document.getElementById('site');
+      frame.src = this.$data.currentPage;
+      this.$data.added[this.$data.selected-1].page = this.$data.currentPage;
+    },
+    doHide: async function() {
+      await minimize(`device_${this.$data.selected}`);
+    },
+  },
+  watch: {
+    selectedPreset: function(selected) {
+      if(this.$data.presets[selected] === undefined) {
+        return;
+      }
+      this.applyPreset(this.$data.presets[selected]);
+    },
+    currentWidth: async function(w) {
+      w=parseInt(w);
+      console.log(this.$data.currentWidth, this.$data.currentHeight);
+      overwolf.windows.changeSize(`device_${this.$data.selected}`, w, this.$data.currentHeight, console.log);
+      const window = (await getOpenWindows())[`device_${this.$data.selected}`];
+      const frame = window.document.getElementById('site');
+      frame.style.width = `${w}px`;
+      this.$data.added[this.$data.selected-1].width = w;
+    },
+    currentHeight: async function(h) {
+      h=parseInt(h);
+      console.log(this.$data.currentWidth, this.$data.currentHeight);
+      overwolf.windows.changeSize(`device_${this.$data.selected}`, this.$data.currentWidth, h, console.log);
+      const window = (await getOpenWindows())[`device_${this.$data.selected}`];
+      const frame = window.document.getElementById('site');
+      frame.style.height = `${h}px`;
+      this.$data.added[this.$data.selected-1].height = h;
+    },
+    currentScale: async function(s) {
+      s=parseInt(s);
+      const window = (await getOpenWindows())[`device_${this.$data.selected}`];
+      const frame = window.document.getElementById('site');
+      frame.style.zoom = this.$data.currentScale;
+      this.$data.added[this.$data.selected-1].scale = s;
+    },
+    selected: function(s) {
+      const item = this.$data.added[s-1];
+      this.$data.currentWidth = item.width;
+      this.$data.currentHeight = item.height;
+      this.$data.currentScale = item.scale;
+      this.$data.currentPage = item.page;
+    }
+  }
+});
---
+++ scripts/ow-async.js
@@ -0,0 +1,172 @@
+/* global overwolf */
+
+async function getCurrentWindow() {
+  return new Promise(resolve => {
+    overwolf.windows.getCurrentWindow(result => {
+      resolve(result.window);
+    });
+  });
+}
+
+async function getWindowState() {
+  return new Promise(resolve => {
+    overwolf.windows.getWindowState(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function getWindow(id) {
+  if (!id) {
+    return getCurrentWindow();
+  }
+  return new Promise(resolve => {
+    overwolf.windows.obtainDeclaredWindow(id, result => {
+      resolve(result.window);
+    });
+  });
+}
+
+async function getOpenWindows() {
+  return new Promise(resolve => {
+    overwolf.windows.getOpenWindows(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function minimize(id) {
+  const window = await getWindow(id);
+  return new Promise(resolve => {
+    overwolf.windows.minimize(window.id, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function maximize(id) {
+  const window = await getWindow(id);
+  return new Promise(resolve => {
+    overwolf.windows.maximize(window.id, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function hide(id) {
+  const window = await getWindow(id);
+  return new Promise(resolve => {
+    overwolf.windows.hide(window.id, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function restore(id) {
+  const window = await getWindow(id);
+  return new Promise(resolve => {
+    overwolf.windows.restore(window.id, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function newWindow(id) {
+  return new Promise(resolve => {
+    overwolf.windows.obtainDeclaredWindow(id, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function close(id) {
+  if (!id) {
+    const currentWindow = await getCurrentWindow();
+    id = currentWindow.id;
+  }
+  overwolf.windows.close(id);
+}
+
+async function dragMove() {
+  // moves a window with the user's mouse
+
+  overwolf.windows.getCurrentWindow(function(result) {
+    if (result.status == "success") {
+      overwolf.windows.dragMove(result.window.id);
+    }
+  });
+}
+
+async function isEGSEnabled() {
+  return new Promise(resolve => {
+    overwolf.egs.isEnabled(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function requestEGS() {
+  return new Promise(resolve => {
+    overwolf.egs.requestToDisplay(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function setEGSStatus(status) {
+  return new Promise(resolve => {
+    overwolf.egs.requestToDisplay(status, result => {
+      resolve(result);
+    });
+  });
+}
+
+async function getManifest() {
+  return new Promise(resolve => {
+    overwolf.extensions.current.getManifest(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function getHotkey() {
+  return new Promise(resolve => {
+    overwolf.settings.getHotKey(result => {
+      resolve(result.hotkey);
+    });
+  });
+}
+
+async function registerHotkey(name, f) {
+  return new Promise(() => {
+    overwolf.settings.registerHotKey(name, result => {
+      if (result.status == "success") {
+        f();
+      }
+    });
+  });
+}
+
+async function getRunningGameInfo() {
+  return new Promise(resolve => {
+    overwolf.games.getRunningGameInfo(result => {
+      resolve(result);
+    });
+  });
+}
+
+async function isOverlayEnabled(id) {
+  return new Promise(resolve => {
+    overwolf.settings.games.getOverlayEnabled(id, result => {
+      resolve(result.enabled);
+    });
+  });
+}
+
+async function isAutoLaunchEnabled(id) {
+  return new Promise(resolve => {
+    overwolf.settings.games.getAutoLaunchEnabled(id, result => {
+      resolve(result.autoLaunchEnabled);
+    });
+  });
+}
